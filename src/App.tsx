522bd47829202aabe77f24c042e0bede839e7384
Gavin King <plumpnation@gmail.com>
2021-05-14 13:53:26 +0200

    Working grapes editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { GrapesjsReact } from 'grapesjs-react';
+import 'grapesjs-preset-webpage';
+import 'grapesjs/dist/css/grapes.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => (
+  <GrapesjsReact
+    id='grapesjs-react'
+    // @ts-ignore
+    plugins={[
+      'gjs-preset-webpage',
+      'gjs-blocks-basic'
+    ]}
+  />
+);
 
 export default App;
